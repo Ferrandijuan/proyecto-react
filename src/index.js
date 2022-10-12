9ba7d65c0c9119f6aac51deef72adca9be1571a1
Juan Ignacio Ferrandi <juanife@live.com.ar>
2022-10-12 20:40:29 -0300

    PreEntrega1-FerrandiJuan

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemePage } from './componentes/ThemePage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemePage>
+      <App />
+    </ThemePage>
   </React.StrictMode>
 );
 
